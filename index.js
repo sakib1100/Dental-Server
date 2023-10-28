722a210c0e0b73137532ded57ac9cd81638845ed
Mostafa Mohsin <mostafamohsin783@gmail.com>
2023-10-27 23:24:08 -0700

    data.json

--- index.js
+++ index.js
@@ -23,6 +23,51 @@ const client = new MongoClient(uri, {
 
 async function run(){
 
+try{
+await client.connect();
+const bookingCollection = client.db('Dental-clinic').collection('bookings');
+const servicesCollection = client.db('Dental-clinic').collection('services');
+
+
+// insert data form booking modal
+
+app.post('/booking', async (req,res) => {
+const booking = req.body;
+ result = await bookingCollection.insertOne(booking);
+res.send(result);
+})
+
+
+
+app.get('/serviceGet', async(req,res) => {
+  const getService = await servicesCollection.find({}).toArray();
+  res.send(getService);
+})
+
+
+
+
+
+// view booking data 
+
+
+app.get('/booking', async(req,res) => {
+  const patients = req.query.patient;
+  const querys = {patent:patients};
+  const booking = await bookingCollection.find(querys).toArray();
+  res.send(booking);
+})
+
+
+
+
+
+}
+
+finally{
+
+}
+
 }
 
 run().catch(console.dir);
