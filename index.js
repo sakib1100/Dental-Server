a6fdc50afa970f73dcdc5acabb16caf0975736ff
Mostafa Mohsin <mostafamohsin783@gmail.com>
2023-10-28 08:39:59 -0700

    dataInert

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.Db_user}:${process.env.Db_pass}@cluster0.ppwrhof.mongodb.net/?retryWrites=true&w=majority`;
@@ -80,4 +80,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Dental app listening on port ${port}`)
-})
+});
